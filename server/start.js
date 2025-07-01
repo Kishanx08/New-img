ca9e7642145f6f73fcd6ffce2f27dfe89ffed7b9
Kishanx08 <kishanx03@proton.me>
2025-07-01 16:14:57 +0530

    Revert "Create start.js"
    This reverts commit 976a13e57ae3bdd6fd356817e6e08e3f4c86ed07.

--- server/start.js
+++
@@ -1,6 +0,0 @@
-const { createServer } = require('./index');
-const app = createServer();
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Backend listening on port ${PORT}`);
-}); 
